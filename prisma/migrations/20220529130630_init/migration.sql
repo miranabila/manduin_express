19d7ae2fb1694077b5d8928aa2d53089eaeeb7f5
GhiyasAsh-Shidiqie <82864367+YashaCamael@users.noreply.github.com>
2022-05-30 11:18:35 +0700

    upload files

---
+++ prisma/migrations/20220529130630_init/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE `landmark` (
+    `land_id` INTEGER NOT NULL,
+    `nama` VARCHAR(50) NULL,
+    `lat` FLOAT NULL,
+    `lon` FLOAT NULL,
+    `distance` FLOAT NULL,
+    `category` VARCHAR(50) NULL,
+    `city` VARCHAR(50) NULL,
+    `price` INTEGER NULL,
+    `rating` INTEGER NULL,
+    `description` TEXT NULL,
+    `Img_Url` TEXT NULL,
+
+    PRIMARY KEY (`land_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `wisata` (
+    `place_id` INTEGER NOT NULL,
+    `land_id` INTEGER NULL,
+    `nama` VARCHAR(50) NULL,
+    `city` VARCHAR(50) NULL,
+    `lat` FLOAT NULL,
+    `lon` FLOAT NULL,
+    `distance` FLOAT NULL,
+    `category` VARCHAR(50) NULL,
+    `price` INTEGER NULL,
+    `rating` INTEGER NULL,
+    `description` TEXT NULL,
+    `Img_Url` TEXT NULL,
+
+    INDEX `land_id`(`land_id`),
+    PRIMARY KEY (`place_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `wisata` ADD CONSTRAINT `wisata_ibfk_1` FOREIGN KEY (`land_id`) REFERENCES `landmark`(`land_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
